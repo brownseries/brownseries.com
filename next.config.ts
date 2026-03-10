135103e4f283913529338b3be786bdd4599a1422
fawazabdulla <fawazabdullakp@gmail.com>
2026-03-11 02:29:18 +0530

    feat: Disable Serwist in non-production environments and enable Turbopack.

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,13 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV !== "production",
 });
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {},
 };
 
 export default withSerwist(nextConfig);
